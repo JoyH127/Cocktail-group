3e0e0c4d0dbf52695883da225934a12c2a4c28e2
JoyH127 <hh495@scarletmail.rutgers.edu>
2022-04-06 10:20:04 -0400

    fix button render

--- src/screens/Cocktails.js
+++ src/screens/Cocktails.js
@@ -1,10 +1,11 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CocktailCard from "../components/CocktailCard";
 
 function Cocktails() {
   const [nonAlcoholics, setNonAlcoholics] = useState([]);
-
+  const [alcoholics, setAlcoholics] = useState([]);
+  const [render, setRender] = useState([]);
   const nonAlcoholicDrinks = async () => {
     try {
       let response = await axios.get(
@@ -13,36 +14,64 @@ function Cocktails() {
       //console.log(response.data.drinks)
       const { drinks } = response.data;
       setNonAlcoholics(drinks);
-      // const newArr = [...nonAlcoholics,response.data.drinks]
-      // console.log(newArr)
-      // setNonAlcoholics(newArr)
-      console.log(nonAlcoholics);
     } catch (error) {
       console.log(error);
     }
   };
 
+  const alcoholicDrinks = async () => {
+    try {
+      let response = await axios.get(
+        `https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic`
+      );
+      //console.log(response.data.drinks)
+      const { drinks } = response.data;
+      setAlcoholics(drinks);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    nonAlcoholicDrinks();
+    alcoholicDrinks();
+  }, []);
+
+  console.log("non", nonAlcoholics);
+  console.log("he", alcoholics);
+
   const handleNonAlcoholicBtn = (e) => {
     e.preventDefault();
-    nonAlcoholicDrinks();
-    renderDrinks();
+    setRender(nonAlcoholics);
   };
 
-  const renderDrinks = () => {
-    return nonAlcoholics.map((nonAlcoholic, index) => {
-      return (
-        <CocktailCard
-          name={nonAlcoholic.strDrink}
-          image={nonAlcoholic.strDrinkThumb}
-        />
-      );
-    });
+  const handleAlcoholicBtn = (e) => {
+    e.preventDefault();
+    setRender(alcoholics);
+    // alcoholicDrinks();
+    // renderAlcoholic();
   };
+
+  // const renderAlcoholic = () => {
+  //   return alcoholics.map((alcoholic, index) => {
+  //     return (
+  //       <CocktailCard
+  //         name={alcoholic.strDrink}
+  //         image={alcoholic.strDrinkThumb}
+  //       />
+  //     );
+  //   });
+  // };
 
   return (
     <div>
       <div>
-        <button>Alcoholic Cocktails</button>
+        <button
+          onClick={(e) => {
+            handleAlcoholicBtn(e);
+          }}
+        >
+          Alcoholic Cocktails
+        </button>
         <button
           onClick={(e) => {
             handleNonAlcoholicBtn(e);
@@ -51,86 +80,28 @@ function Cocktails() {
           Non-Alcoholic Cocktails
         </button>
       </div>
-      <div className="drinks">{renderDrinks()}</div>
+      <div className="nonalcoholic">
+        {render.map((nonAlcoholic, index) => {
+          return (
+            <CocktailCard
+              name={nonAlcoholic.strDrink}
+              image={nonAlcoholic.strDrinkThumb}
+            />
+          );
+        })}
+      </div>
+      <div className="alcoholic">
+        {render.map((alcoholic, index) => {
+          return (
+            <CocktailCard
+              name={alcoholic.strDrink}
+              image={alcoholic.strDrinkThumb}
+            />
+          );
+        })}
+      </div>
     </div>
   );
-=======
-function Cocktails(){
-
- const [nonAlcoholics, setNonAlcoholics] = useState([])
- const [alcoholics, setAlcoholics] = useState([])
-    
-    const nonAlcoholicDrinks = async () => {
-        try {
-            let response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic`)
-            //console.log(response.data.drinks)
-             const { drinks } = response.data
-            setNonAlcoholics(drinks)
-           console.log(nonAlcoholics)
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-
-    const alcoholicDrinks = async () => {
-        try {
-            let response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic`)
-            //console.log(response.data.drinks)
-             const { drinks } = response.data
-            setAlcoholics(drinks)
-        } catch (error) {
-            console.log(error)
-        }
-    }
-    
-    const handleNonAlcoholicBtn = (e)=>{
-       e.preventDefault()
-       nonAlcoholicDrinks()
-       renderNonAlcoholic()
-    }
-   
-    const handleAlcoholicBtn = (e)=>{
-        e.preventDefault()
-        alcoholicDrinks()
-        renderAlcoholic()
-     }
- 
-    const renderNonAlcoholic = () => {
-        return nonAlcoholics.map((nonAlcoholic,index) => {
-            return (
-                <CocktailCard 
-                  name={nonAlcoholic.strDrink}
-                  image={nonAlcoholic.strDrinkThumb}
-                />
-            )
-        })
-    }
-
-    const renderAlcoholic = () => {
-        return alcoholics.map((alcoholic,index) => {
-            return (
-                <CocktailCard 
-                  name={alcoholic.strDrink}
-                  image={alcoholic.strDrinkThumb}
-                />
-            )
-        })
-    }
-
-    return(
-         <div>
-             <div>
-             <button onClick={(e) => {handleAlcoholicBtn(e)}}>Alcoholic Cocktails</button>
-             <button onClick={(e) => {handleNonAlcoholicBtn(e)}}>Non-Alcoholic Cocktails</button>
-            </div>
-            <div className='nonalcoholic'>{renderNonAlcoholic()}</div>
-            <div className='alcoholic'>{renderAlcoholic()}</div>
-         
-         </div>
-       
-    )
->>>>>>> main
 }
 
 export default Cocktails;
