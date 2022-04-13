1592a4c2777813017f9f3ca8080272691695f7d2
Poojitha Vangala <poojithavangala@Poojithas-MacBook-Pro.local>
2022-04-13 18:06:25 -0400

    added categories and cuisines

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,10 +1,14 @@
 import { useNavigate } from "react-router-dom"
 
 function RecipeCard(props){
+
+    console.log(props)
     const navigate = useNavigate()
     return(
         <div>
-       
+           <img src={props.image} alt="category" />
+           {console.log("img", props.image)}
+           <h3>{props.name}</h3>
         </div>
     )
 }
--- src/services/constants.js
+++ src/services/constants.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const fetchData = async(category) => {
+export const fetchData = async(category) => {
     try {
         const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=${category}`)
         //console.log(response)
@@ -10,9 +10,9 @@ const fetchData = async(category) => {
     }
 }
 
-const fetchCategory = async() => {
+export const fetchRecipes = async(list) => {
     try {
-        const res = await axios.get(`https://www.themealdb.com/api/json/v1/1/list.php?c=list`)
+        const res = await axios.get(`https://www.themealdb.com/api/json/v1/1/list.php?${list}`)
         //console.log(res)
         return res
     } catch (error) {
@@ -20,18 +20,22 @@ const fetchCategory = async() => {
     }
 }
 
-const fetchCuisine = async() => {
+export const fetchCategoryList = async(clists) => {
+   try {
+    const res = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${clists}`)
+       //console.log(res)
+       return res
+   } catch (error) {
+       console.log(error)
+   }
+}
+
+export const fetchCuisineList = async(lists) => {
     try {
-        const resp = await axios.get(`https://www.themealdb.com/api/json/v1/1/list.php?a=list`)
-        //console.log(resp)
-        return resp
+     const res = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?a=${lists}`)
+        //console.log(res)
+        return res
     } catch (error) {
         console.log(error)
     }
-}
-
-export {
-    fetchData,
-    fetchCategory,
-    fetchCuisine
-}
+ }
