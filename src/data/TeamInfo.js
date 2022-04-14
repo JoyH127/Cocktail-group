dc391dd91a212472c0f3f326de7c423fc0daf0b7
Poojitha Vangala <poojithavangala@Poojithas-MacBook-Pro.local>
2022-04-14 09:31:37 -0400

    updated css

--- src/data/TeamInfo.js
+++ src/data/TeamInfo.js
@@ -1,15 +1,34 @@
 const team = [
     { 
-    name: {
-    first: "Hee",
-    last: "Hwang"
-     },
-    location: "New Jersey",
-    email: "",
-    GitHub: "",
-    LinkedIn: ""
-    }
-   
+        name: {
+        first: "Hee",
+        last: "Hwang"
+        },
+        location: "New Jersey",
+        email: "",
+        GitHub: "",
+        LinkedIn: ""
+    },
+    { 
+        name: {
+        first: "Lance",
+        last: "Wilshire"
+         },
+        location: "New Jersey",
+        email: "",
+        GitHub: "",
+        LinkedIn: ""
+        },
+    { 
+        name: {
+        first: "Poojitha",
+        last: "Vangala"
+        },
+        location: "New Jersey",
+        email: "",
+        GitHub: "",
+        LinkedIn: ""
+    },
 ] 
 
 export default team
--- src/screens/About.js
+++ src/screens/About.js
@@ -1,12 +1,13 @@
+import team from '../data/TeamInfo'
 
-function About(){
+function AboutUs(){
     return(
-        <div>
-           <h3>Welcome to React Cocktail App!</h3>
+        <div className="aboutus">
+           <h3>Welcome to Our App!</h3>
            <p></p>
            <p>Team info</p>
         </div>
     )
 }
 
-export default About
+export default AboutUs
