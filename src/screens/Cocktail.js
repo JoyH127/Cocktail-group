7a5a843b2d1944551fc0d767f17e8b019376a982
lancecordel <64986141+lancecordel@users.noreply.github.com>
2022-04-14 13:53:30 -0400

    Update Cocktail.js

--- src/screens/Cocktail.js
+++ src/screens/Cocktail.js
@@ -5,7 +5,7 @@ import { useParams } from "react-router-dom";
 
 
 function Cocktail(props) {
-  // const [cocktail, setCocktail] = useState([]);
+  const [cocktail, setCocktail] = useState([]);
   // const [info, setInfo] = useState([]);
 
   const[measurement, setMeasure] = useState([]);
@@ -13,13 +13,14 @@ function Cocktail(props) {
   const[drinkName, setDrinkName] = useState([]);
   const[drinkGlass, setDrinkGlass] = useState([]);
   const[alcoholic, setAlcoholic] = useState([]);
+  const[instruction, setInstruction] = useState([]);
 
   const { name } = useParams();
   async function fetchData() {
     const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${name}`);
     const { drinks } = response.data;
     console.log(drinks)
-    // setCocktail(drinks[0]);
+    setCocktail(drinks[0]);
     const entries = Object.entries(drinks[0]);
     const filterNull = entries.filter((array) => {
       if(!array.includes(null)) return array
@@ -33,24 +34,28 @@ function Cocktail(props) {
       && !array[0].includes('Creative')
       && !array[1].includes('null')) return array;
     });
+    console.log(measurement)
 
     const filterMeasure = filterKey.filter(array => array[0].includes('strMeasure'))
     const filterIngredients = filterKey.filter(array => array[0].includes('strIngredient'))
     const filterName = filterKey.filter(array => array[0].includes('strDrink'))
     const filterGlass = filterKey.filter(array => array[0].includes('strGlass'))
     const filterAlcoholic = filterKey.filter(array => array[0].includes('strAlcoholic'))
+    const filterInstructions = filterKey.filter(array => array[0].includes('strInstruction'))
 
     const strAlcoholic = filterAlcoholic.map(array => array[1])
     const strName = filterName.map(array => array[1])
     const strGlass = filterGlass.map(array => array[1])
     const strMeasure = filterMeasure.map(array => array[1])
     const strIngredient = filterIngredients.map(array => array[1])
+    const strInstruction = filterInstructions.map(array => array[1])
 
     setMeasure(strMeasure);
     setIngredients(strIngredient);
     setDrinkName(strName);
     setDrinkGlass(strGlass);
     setAlcoholic(strAlcoholic);
+    setInstruction(strInstruction);
   }
 
   useEffect(() => {
@@ -71,10 +76,24 @@ function Cocktail(props) {
               width={300}
             />
           </div>
-          <div className="info-allDetails">
-            {info.map((item) => (
-              <li className="cocktail-info-para">{item}</li>
-            ))}
+          <div className="info-details">
+            <p className="name" >{drinkName}</p>
+            <p>served in a {drinkGlass}</p>
+
+            <div className="how-to-make-container">
+              <div className="ingredients">
+              {   ingredient.map(ing => 
+                  <p>{ing}</p> ) }
+              </div>
+              <div className="measurements" >
+                {measurement.map(measure => 
+                  <p>{measure}</p> )}
+              </div>
+            </div>
+
+            <p>{instruction}</p>
+
+
           </div>
         </div>
         </div>
