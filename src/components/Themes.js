127e94591f3d7c6952e630790fc56bf951ceeb59
Poojitha Vangala <poojithavangala@Poojithas-MacBook-Pro.local>
2022-04-15 00:23:40 -0400

    added darkmode

---
+++ src/components/Themes.js
@@ -0,0 +1,12 @@
+export const lightTheme = {
+    body: '#d6cec2',
+    text: '#363537',
+    toggleBorder: '#fff',
+    background: '#d6cec2',
+}
+export const darkTheme = {
+    body: '#363537',
+    text: '#fff',
+    toggleBorder: '#6B8096',
+    background: '#999',
+}
